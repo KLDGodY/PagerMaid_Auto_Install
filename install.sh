bd91e7de5d8bfa3099a36b77ac85711e2e788b43
KLDGodY <53929319+KLDGodY@users.noreply.github.com>
2020-08-30 12:15:17 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,54 +1,59 @@
+[[ $(id -u) != 0 ]] && echo -e "哎呀......请使用 ${red}root ${none}用户运行 ${yellow}~(^_^) ${none}\n" && exit 1
+clear
+
+echo "------PagerMaid Auto Install------"
+echo "1. 开始"
+echo "2. 结束"
+echo "----------------------------------"
+echo 
+read startr
+if [ "$startr" = "2" ] || [ "$startr" != 1 ]; then
+	exit 1
+fi
+
 cd /var/lib
 
-update_and_install(){
-	apt-get update -y
-	apt-get upgrade -y
-	apt-get install sudo -y
-	apt-get install imagemagick -y
-	apt-get install software-properties-common -y
-	add-apt-repository ppa:dawidd0811/neofetch
-	apt-get install neofetch -y
-        apt install tesseract-ocr-all -y
-	apt-get install libzbar-dev -y
-	
-	apt install tesseract-ocr -y
-	if command -v python3.6;then
-		echo 'Python 3.6 存在...'
-		update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 100000000000
-		update-alternatives --config python3
-	else
-		wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
-		tar xvzf Python-3.6.5.tgz
-		gzip -dv Python-3.6.5.tgz
-		tar xvf Python-3.6.5.tar
-		cd Python-3.6.5
-		./configure --enable-optimizations
-		make && make altinstall
-		update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.6 100000000000
-		update-alternatives --config python3
-		python3 -V
-	fi
-	if command -v pip3;
-	then
-	echo 'pip3 存在...'
-	else
-		apt-get install python3-pip -y
-	fi
-	pip3 install email_validator
-	pip3 install zbar
-	pip3 install --upgrade pip
-	sudo -H pip3 install --ignore-installed PyYAML
-	sudo -H pip3 install sentry-sdk==0.16.0
-	apt-get install screen -y
-	
-	if command -v git;then
-		echo 'Git 存在...'
-	else
-		apt-get install git -y
-	fi
-}
+apt-get update -y
+apt-get upgrade -y
+apt-get install sudo -y
+apt-get install imagemagick -y
+apt-get install software-properties-common -y
+add-apt-repository ppa:dawidd0811/neofetch
+apt-get install neofetch -y
+apt install tesseract-ocr-all -y
+apt-get install libzbar-dev -y
 
-update_and_install
+apt install tesseract-ocr -y
+if command -v python3.6;then
+	echo 'Python 3.6 存在...'
+else
+	wget https://www.python.org/ftp/python/3.6.5/Python-3.6.5.tgz
+	tar xvzf Python-3.6.5.tgz
+	gzip -dv Python-3.6.5.tgz
+	tar xvf Python-3.6.5.tar
+	cd Python-3.6.5
+	./configure --enable-optimizations
+	make && make altinstall
+	python3 -V
+fi
+if command -v pip3;
+then
+echo 'pip3 存在...'
+else
+	apt-get install python3-pip -y
+fi
+pip3 install email_validator
+pip3 install zbar
+pip3 install --upgrade pip
+sudo -H pip3 install --ignore-installed PyYAML
+sudo -H pip3 install sentry-sdk==0.16.0
+apt-get install screen -y
+
+if command -v git;then
+	echo 'Git 存在...'
+else
+	apt-get install git -y
+fi
 
 git clone https://github.com/xtaodada/PagerMaid-Modify.git
 cd PagerMaid-Modify
